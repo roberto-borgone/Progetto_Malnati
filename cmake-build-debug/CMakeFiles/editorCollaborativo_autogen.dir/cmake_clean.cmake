e6fb8e8c4d1fc0b0fc949e1b4af94ea086b0aa81
clemcecs <clemente.cetera@studenti.polito.it>
2020-05-10 18:16:03 +0200

    aggiunta progetto server con databases e divisione progetti per client e server

--- cmake-build-debug/CMakeFiles/editorCollaborativo_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/editorCollaborativo_autogen"
-  "editorCollaborativo_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/editorCollaborativo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
